9f20e32847228379304c5db560486c55870dadda
shyam-ramani <shyamramani369@gmail.com>
2025-06-20 12:24:01 +0530

    bgchanger

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 function App() {
-  const [color, setColor] = useState("olive");
+  const [color, setcolor] = useState("olive");
+ 
   return (
-    <div className="w-full h-screen bg-blue-500 relative" style={{ backgroundColor: color }}>
-     <div className="fixed  flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-      <div className="flex flex-wrap justify-center gap-3 px-3 py-2 rounded-3xl">
-        <button className="outline-none px-4  py-1 rounded-full bg-blue-400 text-white" onClick={() => setColor("blue")}>blue</button>
-        <button className="outline-none px-4  py-1 rounded-full bg-blue-400 text-white" onClick={() => setColor("red")}>red</button>
-        <button className="outline-none px-4  py-1 rounded-full bg-blue-400 text-white" onClick={() => setColor("green")}>green</button>
-        <button className="outline-none px-4  py-1 rounded-full bg-blue-400 text-white" onClick={() => setColor("yellow")}>yellow</button>
-        <button className="outline-none px-4  py-1 rounded-full bg-blue-400 text-white" onClick={() => setColor("black")}>black</button>
-        
-       
-      </div>
-     </div>
+    <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+        <div className="fixed flex flexwrap justify-center bottom-12 inset-x-0 px-2">
+         <div className="flex flexwrap justify-center gap-5 rounded-full text-1.5xl px-4 py-2 m-5  bg-white  mx-356 ">
+
+          <button className="rounded bg-red-500 rounded-full  px-4 py-2" onClick={() => setcolor("red")}>red</button>
+          <button className="rounded bg-blue-500 rounded-full  px-4 py-2" onClick={() => setcolor("blue")}>blue</button>
+          <button className="rounded bg-black  rounded-full px-4 py-2 text-white" onClick={() => setcolor("black")}>black</button>
+          <button className="rounded bg-yellow-500 rounded-full  px-4 py-2" onClick={() => setcolor("yellow")  }>yellow</button>
+          
+         </div>
+
+        </div>
     </div>
+   
   );
 }
 
